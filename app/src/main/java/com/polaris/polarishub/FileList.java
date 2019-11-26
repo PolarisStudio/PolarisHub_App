d427471f063a3b2ec0547dbf8c83c076392d9815
Deslate <deslate@outlook.com>
2019-11-26 09:22:27 +0800

    add 'select file'function

--- app/src/main/java/com/polaris/polarishub/FileList.java
+++ app/src/main/java/com/polaris/polarishub/FileList.java
@@ -1,16 +1,24 @@
 package com.polaris.polarishub;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,7 +39,12 @@ import com.polaris.polarishub.Tools.TDPitem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -56,7 +69,21 @@ public class FileList extends AppCompatActivity {
         
         initItems();
         TdpAdapter adapter=new TdpAdapter(FileList.this,R.layout.tdp_item,items,mListener1,mListener2);
+
+        Button selectFile = (Button)findViewById(R.id.select_file_butt );
+        selectFile.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+				intent.setType("*/*");//无类型限制
+				intent.addCategory(Intent.CATEGORY_OPENABLE);
+				startActivityForResult(intent, 1);
+			}
+		}) ;
+
+
         ListView dailyList=(ListView)findViewById(R.id.daily_list);
+
         dailyList.setAdapter(adapter);
         
         dailyList.setOnItemClickListener(new OnItemClickListener(){
@@ -110,25 +137,158 @@ public class FileList extends AppCompatActivity {
         dailyList.setOnItemLongClickListener(new OnItemLongClickListener(){
         	@Override
         	public boolean onItemLongClick(AdapterView<?> parent,View view,int position,long id){
-        		//暂且保留这段代码！！
-				//长按进入编辑状态，但是还没想好要编辑啥
-        		/*selectedPosition=position;
+        		selectedPosition=position;
         		
         		TDPitem item=items.get(position);
         		item.setState(TDPitem.MANAGE);
-        		selectedId=item.getItemId();
+        		selectedFile=item.getFile();
         		
         		TdpAdapter adapter=new TdpAdapter(FileList.this,R.layout.tdp_item,items,mListener1,mListener2);
                 ListView dailyList=(ListView)findViewById(R.id.daily_list);
                 dailyList.setAdapter(adapter);
 
-        		 */
-                
+
                 return true;
         	}
         });
         
     }
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK) {
+			try{
+				String originPath;
+				String fileName;
+				Uri uri = data.getData();
+				System.out.println(uri);
+				originPath = getPath(this, uri);
+				fileName = originPath.substring(originPath.lastIndexOf("/") + 1);
+				Toast.makeText(this,"已将"+fileName+"拷贝至同步文件夹",Toast.LENGTH_LONG).show();
+				File originFile = new File(originPath);
+				File polarisHubFolder =new File(Environment.getExternalStorageDirectory(),"PolarisHub");
+				while(!polarisHubFolder.exists()){
+					polarisHubFolder.mkdir();
+					//if(com.des.butler.MainActivity.DeveloperState==true){Toast.makeText(ItemEdit.this, "����Butler��"+Environment.getExternalStorageDirectory(), Toast.LENGTH_SHORT).show();}
+				}
+				FileInputStream fileInputStream = new FileInputStream(originFile);
+				FileOutputStream fileOutputStream = new FileOutputStream(polarisHubFolder+"/"+fileName);
+				byte[] buffer = new byte[1024];
+				int byteRead;
+				while (-1 != (byteRead = fileInputStream.read(buffer))) {
+					fileOutputStream.write(buffer, 0, byteRead);
+				}
+				fileInputStream.close();
+				fileOutputStream.flush();
+				fileOutputStream.close();
+				reinitItems();
+				TdpAdapter adapter=new TdpAdapter(FileList.this,R.layout.tdp_item,items,mListener1,mListener2);
+				ListView dailyList=(ListView)findViewById(R.id.daily_list);
+				dailyList.setAdapter(adapter);
+			}catch(Exception e){
+
+			}
+		}
+	}
+	public String getPath(final Context context, final Uri uri) {//处理文件路径
+
+		final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+		// DocumentProvider
+		if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+			// ExternalStorageProvider
+			if (isExternalStorageDocument(uri)) {
+				final String docId = DocumentsContract.getDocumentId(uri);
+				final String[] split = docId.split(":");
+				final String type = split[0];
+
+				if ("primary".equalsIgnoreCase(type)) {
+					return Environment.getExternalStorageDirectory() + "/" + split[1];
+				}
+			}
+			// DownloadsProvider
+			else if (isDownloadsDocument(uri)) {
+
+				final String id = DocumentsContract.getDocumentId(uri);
+				final Uri contentUri = ContentUris.withAppendedId(
+						Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+				return getDataColumn(context, contentUri, null, null);
+			}
+			// MediaProvider
+			else if (isMediaDocument(uri)) {
+				final String docId = DocumentsContract.getDocumentId(uri);
+				final String[] split = docId.split(":");
+				final String type = split[0];
+
+				Uri contentUri = null;
+				if ("image".equals(type)) {
+					contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+				} else if ("video".equals(type)) {
+					contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+				} else if ("audio".equals(type)) {
+					contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+				}
+
+				final String selection = "_id=?";
+				final String[] selectionArgs = new String[]{split[1]};
+
+				return getDataColumn(context, contentUri, selection, selectionArgs);
+			}
+		}
+		// MediaStore (and general)
+		else if ("content".equalsIgnoreCase(uri.getScheme())) {
+			return getDataColumn(context, uri, null, null);
+		}
+		// File
+		else if ("file".equalsIgnoreCase(uri.getScheme())) {
+			return uri.getPath();
+		}
+		return null;
+	}
+	public String getDataColumn(Context context, Uri uri, String selection,
+								String[] selectionArgs) {
+
+		Cursor cursor = null;
+		final String column = "_data";
+		final String[] projection = {column};
+
+		try {
+			cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+					null);
+			if (cursor != null && cursor.moveToFirst()) {
+				final int column_index = cursor.getColumnIndexOrThrow(column);
+				return cursor.getString(column_index);
+			}
+		} finally {
+			if (cursor != null)
+				cursor.close();
+		}
+		return null;
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is ExternalStorageProvider.
+	 */
+	public boolean isExternalStorageDocument(Uri uri) {
+		return "com.android.externalstorage.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is DownloadsProvider.
+	 */
+	public boolean isDownloadsDocument(Uri uri) {
+		return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is MediaProvider.
+	 */
+	public boolean isMediaDocument(Uri uri) {
+		return "com.android.providers.media.documents".equals(uri.getAuthority());
+	}
 	private TdpAdapter.MyClickListener mListener1 = new TdpAdapter.MyClickListener() {
 		@Override
 		public void myOnClick(/*int position,*/ View v) {
@@ -145,7 +305,11 @@ public class FileList extends AppCompatActivity {
     private TdpAdapter.MyClickListener mListener2 = new TdpAdapter.MyClickListener() {
 		         @Override
 		       public void myOnClick(View v) {
-		        	 //实现按钮 1 的逻辑
+		        	 selectedFile.delete();
+					 reinitItems();
+					 TdpAdapter adapter=new TdpAdapter(FileList.this,R.layout.tdp_item,items,mListener1,mListener2);
+					 ListView dailyList=(ListView)findViewById(R.id.daily_list);
+					 dailyList.setAdapter(adapter);
 		         }
 		     };
 	private void initItems(){
@@ -156,7 +320,8 @@ public class FileList extends AppCompatActivity {
 			File currentFile = filelist[current];
 			int id=-1;//cursor.getInt(cursor.getColumnIndex("id"));//没有用
 			String title=currentFile.getName();
-			String detail="detail";
+			String type = currentFile .getName().substring(currentFile .getName().lastIndexOf(".") + 1);
+			String detail="文件类型："+type;
 			int imageId=0;
 			String time="no time";
 			if (imageId==R.drawable.basic_file){
@@ -169,6 +334,14 @@ public class FileList extends AppCompatActivity {
 		}
     }
 	
-
+    public void reinitItems(){
+		File polarisHubFolder =new File(Environment.getExternalStorageDirectory(),"PolarisHub");
+		while(!polarisHubFolder.exists()){
+			System.out.println("Download:"+polarisHubFolder.exists() );
+			polarisHubFolder.mkdir();
+		}
+		filelist = polarisHubFolder.listFiles();//重新扫描文件夹
+		initItems();
+	}
 
 }
